f19920083482795c23125ec238756d37582ebe54
Sony <syno3311@gmail.com>
2022-05-25 10:08:27 +0700

    update-folder

---
+++ src/app/shared/models/request/doctor.request.ts
@@ -0,0 +1,5 @@
+export namespace DoctorRequest {
+  export interface CreateDoctor {}
+
+  export interface UpdateDoctor {}
+}
---
+++ src/app/shared/models/response/doctor.response.ts
@@ -0,0 +1,10 @@
+import { BaseResponseModel } from '../base.model';
+import { DoctorModel } from '../doctor.model';
+
+export namespace DoctorResponse {
+  export interface GetDoctors
+    extends BaseResponseModel<DoctorModel.DoctorInfo[]> {}
+
+  export interface CreateDoctor
+    extends BaseResponseModel<DoctorModel.DoctorInfo> {}
+}
